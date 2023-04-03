d85d91accdc4cf3ec03fe1c8ed99fc50f3689380
Nguyễn Xuân Tuấn <tuannx@bap.jp>
2023-04-03 10:31:24 +0700

    fix payment

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -21,7 +21,6 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
     mode: 'payment',
     line_items: [
       {
-       
         price_data: {
           currency: 'usd',
           unit_amount: tour.price * 100,
@@ -29,8 +28,10 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
             name: `${tour.name} Tour`,
             description: tour.summary,
             images: [
-                `${req.protocol}://${req.get('host')}/img/tours/${tour.imageCover}`
-              ],
+              `${req.protocol}://${req.get('host')}/img/tours/${
+                tour.imageCover
+              }`
+            ]
           }
         },
         quantity: 1
@@ -55,10 +56,10 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 const createBookingCheckout = async session => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
-  const price = session.display_items[0].amount / 100;
+  const price = session.amount_total / 100;
   await Booking.create({ tour, user, price });
 };
-exports.webhookCheckout = catchAsync(async (req, res, next) => {
+exports.webhookCheckout = (req, res, next) => {
   const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
   const signature = req.headers['stripe-signature'];
   let event;
@@ -74,9 +75,9 @@ exports.webhookCheckout = catchAsync(async (req, res, next) => {
   console.log(event.type);
 
   if (event.type === 'checkout.session.complete')
-    await createBookingCheckout(event.data.object);
+    createBookingCheckout(event.data.object);
   res.status(200).json({ received: true });
-});
+};
 
 exports.createBookingCheckout = factory.createOne(Booking);
 exports.getBooking = factory.getOne(Booking);
